8f6109a9b82c272d7c9d894d42b661d02e21a14a
Joe Sweeney <joe@joesweeney.org>
2017-09-14 16:57:56 -0400

    Add functions to read and write arbitrary data

--- src/data.c
+++ src/data.c
@@ -1,10 +1,54 @@
 #include<stdio.h>
 
 
+int write(char *id, int byteCount, char *data) {
+  FILE* fd = fopen(id, "wb");
 
+  if(!fd) {
+    return 0;
+  }
+
+  fwrite(data, 1, byteCount, fd);
+  fclose(fd);
+
+  return 1;
+}
+
+int read(char *id, char buffer[]) {
+  FILE* fd = fopen(id, "rb");
+
+  if(!fd) {
+    return 0;
+  }
+
+  while(1) {
+    int bytes_read = fread(buffer++, 1, 1, fd);
+    if(bytes_read < 1) 
+      break;
+  }
+    
+  
+  /* fwrite(data, 1, byteCount, fd); */
+
+  fclose(fd);
+
+  return 1;
+}
 
 int main()
 {
-    printf("Hello World");
+
+  printf("Testing read and write.\n");
+  
+  char* data = "We made it.";
+
+  write("test1", 12, data);
+
+  char buffer[100];
+
+  read("test1", buffer);
+
+  printf("%s", buffer);
+  printf("\n");
 
 }
--- todo.md
+++ todo.md
@@ -1,6 +1,4 @@
 # TODO
 
-* Write some data to a file
-* Read data from a file
 * Hash some data
 * Make a hash table
