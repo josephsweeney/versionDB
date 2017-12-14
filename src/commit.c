ae685135ec6e1710698d500b88af3a66bd007498
Joe Sweeney <joe@joesweeney.org>
2017-12-14 11:25:40 -0500

    Abstract out a function to get current time

--- src/commit.c
+++ src/commit.c
@@ -3,6 +3,14 @@
 #include <stdio.h>
 #include "commit.h"
 
+u64 get_time() {
+  struct timeval tp;
+  gettimeofday(&tp, NULL);
+  u64 ms = tp.tv_sec * 1000 + tp.tv_usec / 1000;
+  printf("ms: %llu\n", ms);
+  return ms;
+}
+
 void commit_init(Commit *commit, BYTE data[], BYTE parent[]) {
   commit->has_parent = 0;
 
@@ -12,10 +20,7 @@ void commit_init(Commit *commit, BYTE data[], BYTE parent[]) {
     commit->has_parent = 1;
   } 
 
-  struct timeval tp;
-  gettimeofday(&tp, NULL);
-  u64 ms = tp.tv_sec * 1000 + tp.tv_usec / 1000;
-  printf("ms: %llu\n", ms);
-  
+  u64 ms = get_time();
+
   commit->time = ms;
 }
